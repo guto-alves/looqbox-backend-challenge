b4e5f097810ffa444d144721cfa14d2eb6a0b5c2
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2021-08-22 23:11:28 -0300

    Add initial project

---
+++ src/test/java/com/looqbox/backendchallenge/LooqboxBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looqbox.backendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LooqboxBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
