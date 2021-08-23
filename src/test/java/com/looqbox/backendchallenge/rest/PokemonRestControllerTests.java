85d9c48c350919078006fca95b13af56e163a3d9
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2021-08-23 03:35:06 -0300

    Update PokemonRestControllerTests

--- src/test/java/com/looqbox/backendchallenge/rest/PokemonRestControllerTests.java
+++ src/test/java/com/looqbox/backendchallenge/rest/PokemonRestControllerTests.java
@@ -3,6 +3,7 @@ package com.looqbox.backendchallenge.rest;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,7 +25,24 @@ public class PokemonRestControllerTests {
 		mockMvc.perform(get("/pokemons?q={query}", "bu"))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("count", is(9)))
-			.andExpect(jsonPath("results", hasSize(9)));
+			.andExpect(jsonPath("results", hasSize(9)))
+			.andExpect(jsonPath("results[0].name", is("budew")))
+			.andExpect(jsonPath("results[0].highlight", is("<pre>bu</pre>dew")));
+		
+		mockMvc.perform(get("/pokemons?q={query}&order={order}", "pi", "length"))
+			.andExpect(status().isOk())
+			.andDo(print())
+			.andExpect(jsonPath("count", is(29)))
+			.andExpect(jsonPath("results", hasSize(29)))
+			.andExpect(jsonPath("results[9].name", is("pikachu")))
+			.andExpect(jsonPath("results[9].highlight", is("<pre>pi</pre>kachu")));
+		
+		mockMvc.perform(get("/pokemons?q={query}&order={order}", "pi", "alphabetical"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("count", is(29)))
+			.andExpect(jsonPath("results", hasSize(29)))
+			.andExpect(jsonPath("results[7].name", is("pikachu")))
+			.andExpect(jsonPath("results[7].highlight", is("<pre>pi</pre>kachu")));
 	}
 	
 	@Test
@@ -38,6 +56,10 @@ public class PokemonRestControllerTests {
 	@Test
 	public void testGetPokemonsError() throws Exception {
 		mockMvc.perform(get("/pokemons"))
+			.andExpect(status().isBadRequest());
+		
+		mockMvc.perform(get("/pokemons?q={query}&order={order}", "bu", "InvalidOrder"))
+			.andDo(print())
 			.andExpect(status().isBadRequest());
 	}
 	
